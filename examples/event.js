debc06d17f556547d37fd3de6b0e2ed0c9dd2eb5
shumaojie <yanjie.jw@163.com>
2016-07-07 16:48:22 +0800

    updae

---
+++ examples/event.js
@@ -0,0 +1,16 @@
+var  EventEmitter=require('events').EventEmitter;
+var event=new EventEmitter();
+
+event.on("some_event",function()
+{
+   console.log("some_event occured.");
+
+
+}
+
+	);
+
+setTimeout(
+function(){event.emit('some_event')},1000
+
+	);
